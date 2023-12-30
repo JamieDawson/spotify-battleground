ec6d286dea6676cffa83d230d36a8a8a4c221e65
Jamie Dawson <jamierobertdawson@gmail.com>
2023-12-29 22:03:31 -0800

    can get album selection and new album ID with submit button

--- src/Pages/MainPage.jsx
+++ src/Pages/MainPage.jsx
@@ -1,14 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import BandcampPlayer from "react-bandcamp";
 import "../Pages/MainPage.styles.css";
 
 const MainPage = () => {
+  const [albums, album1Update] = useState({
+    ablum1: "3114440086",
+    album2: "1387779480",
+  });
+
+  const [name, setName] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newAlbumID = e.target[0].value;
+    const form = e.target;
+    const formData = new FormData(form);
+
+    // Or you can work with it as a plain object:
+    const formJson = Object.fromEntries(formData.entries());
+    console.log(formJson);
+    console.log(newAlbumID);
+  };
+
   return (
     <>
+      <form onSubmit={handleSubmit}>
+        <input onChange={(e) => setName(e.target.value)} value={name}></input>
+        <label>
+          <input type="radio" name="myRadio" value="option1" /> Option 1
+        </label>
+        <label>
+          <input
+            type="radio"
+            name="myRadio"
+            value="option2"
+            defaultChecked={true}
+          />{" "}
+          Option 2
+        </label>
+        <label>
+          <input type="radio" name="myRadio" value="option3" /> Option 3
+        </label>
+        <br />
+        <button type="submit">Click to submit</button>
+      </form>
+
       <div class="container">
         <BandcampPlayer
           BandcampPlayer
-          album="3114440086"
+          album={albums.ablum1}
           bgcol="blue"
           linkcol="blue"
           size="medium"
@@ -17,7 +57,7 @@ const MainPage = () => {
 
         <BandcampPlayer
           BandcampPlayer
-          album="3114440086"
+          album={albums.album2}
           bgcol="blue"
           linkcol="blue"
           size="medium"
@@ -68,5 +108,10 @@ export default MainPage;
 /*
 NPM package used
 https://www.npmjs.com/package/react-spotify-player
+https://www.npmjs.com/package/react-bandcamp
+*/
+
+/*
+Create input that sends album number to function with button click
 
 */
